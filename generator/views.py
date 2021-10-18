6934f7f7da97071e585063bef918a543ba7d69e8
Gerard Naughton <gerardnaughton7@gmail.com>
2021-10-17 20:10:11 -0500

    Added additional password criteria and functionality. Also added about page

--- generator/views.py
+++ generator/views.py
@@ -11,7 +11,17 @@ def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    if request.GET.get('special'):
+        characters.extend(list('#@$%&*!'))
+
+
+    length = int(request.GET.get('length',12))
 
     thepassword = ''
     for x in range(length):
@@ -19,3 +29,6 @@ def password(request):
 
 
     return render(request, 'generator/password.html', {'password':thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
